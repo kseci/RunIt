910e6eba0a2cb4747dceac76613290dc08a03d89
kseci <df_s@hotmail.com>
2015-08-25 14:54:41 +0200

    Deck with array

---
+++ ConsoleApplication1/ConsoleApplication1/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Card
+    {
+        public String face { get; set; }
+        public String suit { get; set; }
+
+        public Card(String face, String suit)
+        {
+            this.face = face;
+            this.suit = suit;
+        }
+        public override string ToString()
+        {
+            return face + " of " + suit;
+        }
+    }
+}
